ef4b73f26637488c29ac7a90ed5191451b37bf11
Shruthi Kannan <112308194+sk28352@users.noreply.github.com>
2023-06-08 15:18:23 +0530

    Add files via upload

---
+++ routes/users.js
@@ -0,0 +1,8 @@
+
+import express from 'express';
+import {getUserInfo, updateUser}  from '../controllers/user.js';
+
+const router = express.Router();
+router.get('/me', getUserInfo);
+router.put('/me', updateUser);
+export default router;
