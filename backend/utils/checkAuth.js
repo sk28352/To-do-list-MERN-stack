07374e1121ce7b9280d5d906cdf83bc7a35e47d7
Shruthi Kannan <112308194+sk28352@users.noreply.github.com>
2023-06-08 15:35:37 +0530

    Add files via upload

---
+++ backend/utils/checkAuth.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+import createError from './createError.js';
+export default  (req,res,next) =>
+{
+    const token = req.cookies.access_token;
+    if(!token){
+        return next(createError({ status: 401, message: 'Unauthorized'})); 
+
+    }
+    return jwt.verify(token,"palace", (err, decoded) => {
+        if(err){
+            return next(createError({ status: 401, message:'Invalid Token'}));
+        }
+            req.user = decoded;
+            return next();
+        
+        
+    });
+};
